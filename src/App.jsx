63f8b6dfd66b4bec54e2477719828e475b4b5ea6
Soufiane CHAOUFI <soufiane.chaoufi@gmail.com>
2021-10-10 11:17:11 -0700

    useQuery data,isLoading,isError,error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const POKEMON_API = "https://pokeapi.co/api/v2/pokemon";
+
+const fetchPokemon = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return axios.get(POKEMON_API).then((res) => res.data.results);
+};
+
 function App() {
-  return <div className='App'>App</div>;
+  const { isSuccess, isLoading, isError, error, data } = useQuery("pokemon", fetchPokemon);
+  if (isLoading) return "Loading....";
+  if (isError) return <div>{error.message}</div>;
+  return <div>{isSuccess && data.map((pokemon) => <div>{pokemon.name}</div>)}</div>;
 }
 
 export default App;
