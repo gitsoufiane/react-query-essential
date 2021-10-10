4aaf7fa2eaef1dbdfec7d39be1b187ef45357fb9
Soufiane CHAOUFI <soufiane.chaoufi@gmail.com>
2021-10-10 12:58:23 -0700

    cahceTime

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 
@@ -8,10 +9,11 @@ const fetchPokemon = async () => {
   return axios.get(POKEMON_API).then((res) => res.data.results);
 };
 
-function App() {
+function Pokemon() {
   const { isSuccess, isLoading, isError, error, data, isFetching } = useQuery("pokemon", fetchPokemon, {
     refetchOnWindowFocus: true,
     staleTime: 5000, // query considered fresh for 5 seconds
+    cacheTime: 2000, // daata remain in cache for N seconds or Infinity or 0
   });
   if (isLoading) return "Loading....";
   if (isError) return <div>{error.message}</div>;
@@ -25,6 +27,15 @@ function App() {
       </div>
     );
   }
+}
+function App() {
+  const [show, setShow] = React.useState(true);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>show</button>
+      {show ? <Pokemon /> : null}
+    </>
+  );
 }
 
 export default App;
