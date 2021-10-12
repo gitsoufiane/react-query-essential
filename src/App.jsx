6bcff0b851bf60a2461b7cddb0ac045fd7ab5fd7
Soufiane CHAOUFI <soufiane.chaoufi@gmail.com>
2021-10-11 20:10:52 -0700

    custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,15 @@ const fetchPokemon = async () => {
   return axios.get(POKEMON_API).then((res) => res.data.results);
 };
 
-function Pokemon({ queryKey }) {
-  const { isSuccess, isLoading, isError, error, data, isFetching } = useQuery(queryKey, fetchPokemon, {
+const usePokemon = () => {
+  return useQuery(['pokemon'], fetchPokemon, {
     refetchOnWindowFocus: true,
     staleTime: 5000, // query considered fresh for 5 seconds
     cacheTime: 2000, // daata remain in cache for N seconds or Infinity or 0
   });
+}
+function Pokemon() {
+  const { isSuccess, isLoading, isError, error, data, isFetching } = usePokemon()
   if (isLoading) return "Loading....";
   if (isError) return <div>{error.message}</div>;
   if (isSuccess) {
@@ -28,11 +31,18 @@ function Pokemon({ queryKey }) {
     );
   }
 }
+
+function Count() {
+  const { isSuccess,  data } = usePokemon()
+
+  return <h3>Pokemon Count : {isSuccess && data.length}</h3>
+}
 function App() {
   const [show, setShow] = React.useState(true);
   return (
     <>
       <button onClick={() => setShow(!show)}>show</button>
+      <Count/>
       {show ? <Pokemon queryKey='pokemon1' /> : null}
     </>
   );
